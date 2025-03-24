2af05efaf190343838f10e687f338d7b804e91a1
vinhshindo123 <vinhnguyenquang71@gmail.com>
2025-03-24 12:44:42 +0700

    Update tracnghiem.js

--- tracnghiem.js
+++ tracnghiem.js
@@ -53,6 +53,7 @@ testSelect.addEventListener('change', function () {
       correct = 0;
       saved_incorrect_text = {}
       quizQuestions.forEach((item) => item.classList.remove("active"));
+      document.querySelector(".quiz_question ul").style.display = 'flex';
       quizAnswers.forEach((item) => {
         item.classList.remove("active");
         item.classList.remove("incorrect");
